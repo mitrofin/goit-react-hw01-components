fe30bb52058df81605e51a0f8b79acac4087c94d
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-05-30 14:53:09 +0300

    add profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+function Profile({ name, tag, location, avatar, stats }) {
+  return (
+    <div class={s.profile}>
+      <div class={s.description}>
+        <img src={avatar} alt={name} class={s.avatar} width="310px" />
+        <p class={s.name}>{name}</p>
+        <p class={s.tag}>{tag}</p>
+        <p class={s.location}>{location}</p>
+      </div>
+
+      <ul class={s.stats}>
+        <li>
+          <span class={s.label}>Followers</span> <br></br>
+          <span class={s.quantity}> {stats.followers}</span>
+        </li>
+        <li>
+          <span class={s.label}>Views</span>
+          <br></br>
+          <span class={s.quantity}> {stats.views}</span>
+        </li>
+        <li>
+          <span class={s.label}>Likes</span>
+          <br></br>
+          <span class={s.quantity}> {stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+Profile.defaultProps = {
+  avatar: 'https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg',
+};
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number.isRequired).isRequired,
+};
+
+export default Profile;
