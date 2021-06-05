4575390d69460a5cdb2633130f47003748e7052d
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-06-05 22:18:56 +0300

    Update TransactionHistory.js

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -3,27 +3,32 @@ import PropTypes from 'prop-types';
 import s from './Transaction.module.scss';
 
 function TransactionHistory({ items }) {
-  <table class="transaction-history">
+  <table className={s.transactionHistory}>
     <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
+      <tr className={s.tableHead}>
+        <th className={s.tableHeader}>Type</th>
+        <th className={s.tableHeader}>Amount</th>
+        <th className={s.tableHeader}>Currency</th>
       </tr>
     </thead>
-
     <tbody>
-      <tr>
-        <td>Invoice</td>
-        <td>125</td>
-        <td>USD</td>
-      </tr>
-      <tr>
-        <td>Withdrawal</td>
-        <td>85</td>
-        <td>USD</td>
+      <tr className={idx}>
+        <td className={s.type}>Invoice</td>
+        <td className={s.amount}>125</td>
+        <td className={s.currency}>USD</td>
       </tr>
     </tbody>
   </table>;
 }
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 export default TransactionHistory;
